3cb9a0f8ec2ff89fdf3850ffad10b464ea5514fe
Adrian Lübke <aluebke@ai-ag.de>
2022-11-16 13:58:08 +0100

    Added age constraint

--- homework_1.sql
+++ homework_1.sql
@@ -45,13 +45,13 @@ CREATE TABLE learning_groups
 CREATE TABLE students
 (
     id                     INT IDENTITY,
-    surname                VARCHAR(50),
+    firstname              VARCHAR(50),
     lastname               VARCHAR(50) NOT NULL,
     gender                 CHAR(1)     NOT NULL,
     birt_date              DATE        NOT NULL,
-    mark_pol               DECIMAL(1, 1),
-    mark_german            DECIMAL(1, 1),
-    mark_english           DECIMAL(1, 1),
+    mark_pol               DECIMAL(1, 0), -- This was (1, 1) ?
+    mark_german            DECIMAL(1, 0), -- This was (1, 1) ?
+    mark_english           DECIMAL(1, 0), -- This was (1, 1) ?
     days_missing           DECIMAL(3, 0) DEFAULT 0,
     days_missing_unexcused DECIMAL(3, 0) DEFAULT 0,
     city                   VARCHAR(50),
@@ -63,3 +63,37 @@ CREATE TABLE students
     CONSTRAINT fk_learning_group
         FOREIGN KEY (learning_group_id) REFERENCES learning_groups (id),
 );
+
+INSERT INTO classes
+VALUES ('Datenbankprogrammierung');
+INSERT INTO education_courses
+VALUES ('Informatik');
+INSERT INTO teacher
+VALUES ('Max', 'Musterman', 1, 1)
+INSERT INTO learning_groups
+VALUES ('INF Studenten', 1, 1)
+
+INSERT INTO students
+VALUES (N'Adrian', N'Luebke', 'm', '2002-11-04',
+        1, 1, 1, 0, 0,
+        N'Berlin', '22334', 'SQL Str.', 6,1);
+
+CREATE FUNCTION dbo.calculate_age(@birth_date DATE)
+    RETURNS INT AS
+BEGIN
+    RETURN DATEDIFF(YY, @birth_date, GETDATE())
+END;
+
+-- Test of my new function
+SELECT firstname, lastname, dbo.calculate_age(birt_date) age FROM students;
+
+ALTER TABLE students
+    ADD CONSTRAINT CHK_student_is_of_eighteen
+    CHECK (dbo.calculate_age(birt_date) >= 18);
+
+-- Testing my constraint
+INSERT INTO students
+VALUES (N'Adrian', N'Luebke', 'm', '2008-11-04',
+        1, 1, 1, 0, 0,
+        N'Berlin', '22334', 'SQL Str.', 6, 1);
+-- INSERT throws error, YEY!
