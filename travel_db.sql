9ab5b0bf53a0ddc05fd729b54f60d75eb6dee43f
Adrian Lübke <aluebke@ai-ag.de>
2022-11-28 16:57:34 +0100

    wip travel db -> added constraint names; renamed trip to journey

--- travel_db.sql
+++ travel_db.sql
@@ -1,21 +1,25 @@
-DROP TABLE IF EXISTS trip;
-DROP TABLE IF EXISTS trip_companion;
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS journey;
+DROP TABLE IF EXISTS journey_companion;
 DROP TABLE IF EXISTS transport_device;
 DROP TABLE IF EXISTS country;
 DROP TABLE IF EXISTS city;
-DROP TABLE IF EXISTS trip_x_trip_companion;
-DROP TABLE IF EXISTS trip_x_transport_device;
-DROP TABLE IF EXISTS trip_x_city;
-DROP TABLE IF EXISTS trip_x_country;
+DROP TABLE IF EXISTS journey_x_journey_companion;
+DROP TABLE IF EXISTS journey_x_transport_device;
+DROP TABLE IF EXISTS journey_x_city;
+DROP TABLE IF EXISTS journey_x_country;
 
-CREATE TABLE trip
+SET FOREIGN_KEY_CHECKS = 1;
+
+CREATE TABLE journey
 (
-    id         INT PRIMARY KEY AUTO_INCREMENT,
-    trip_begin DATE,
-    trip_end   DATE
+    id            INT PRIMARY KEY AUTO_INCREMENT,
+    journey_begin DATE,
+    journey_end   DATE
 );
 
-CREATE TABLE trip_companion
+CREATE TABLE journey_companion
 (
     id         INT PRIMARY KEY AUTO_INCREMENT,
     first_name varchar(20),
@@ -40,42 +44,44 @@ CREATE TABLE country
 
 CREATE TABLE city
 (
-    id        INT PRIMARY KEY AUTO_INCREMENT,
-    city_name VARCHAR(50) NOT NULL
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    city_name  VARCHAR(50) NOT NULL,
+    country_id INT         NOT NULL,
+    CONSTRAINT city_country_fk FOREIGN KEY (country_id) REFERENCES country (id)
 );
 
-CREATE TABLE trip_x_trip_companion
+CREATE TABLE journey_x_journey_companion
 (
-    id                INT PRIMARY KEY AUTO_INCREMENT,
-    trip_id           INT NOT NULL,
-    trip_companion_id INT NOT NULL,
-    FOREIGN KEY (trip_id) REFERENCES trip(id),
-    FOREIGN KEY (trip_companion_id) REFERENCES trip_companion(id)
+    id                   INT PRIMARY KEY AUTO_INCREMENT,
+    journey_id           INT NOT NULL,
+    journey_companion_id INT NOT NULL,
+    CONSTRAINT journey_x_journey_companion_journey_fk FOREIGN KEY (journey_id) REFERENCES journey (id),
+    CONSTRAINT journey_x_journey_companion_journey_companion_fk FOREIGN KEY (journey_companion_id) REFERENCES journey_companion (id)
 );
 
-CREATE TABLE trip_x_transport_device
+CREATE TABLE journey_x_transport_device
 (
     id                  INT PRIMARY KEY AUTO_INCREMENT,
-    trip_id             INT NOT NULL,
+    journey_id          INT NOT NULL,
     transport_device_id INT NOT NULL,
-    FOREIGN KEY (trip_id) REFERENCES trip(id),
-    FOREIGN KEY (transport_device_id) REFERENCES transport_device(id)
+    CONSTRAINT journey_x_transport_journey_fk FOREIGN KEY (journey_id) REFERENCES journey (id),
+    CONSTRAINT journey_x_transport_transport_device_fk FOREIGN KEY (transport_device_id) REFERENCES transport_device (id)
 );
 
-CREATE TABLE trip_x_country
+CREATE TABLE journey_x_country
 (
     id         INT PRIMARY KEY AUTO_INCREMENT,
-    trip_id    INT NOT NULL,
+    journey_id INT NOT NULL,
     country_id INT NOT NULL,
-    FOREIGN KEY (trip_id) REFERENCES trip(id),
-    FOREIGN KEY (country_id) REFERENCES country(id)
+    CONSTRAINT journey_x_country_journey_fk FOREIGN KEY (journey_id) REFERENCES journey (id),
+    CONSTRAINT journey_x_country_country_fk FOREIGN KEY (country_id) REFERENCES country (id)
 );
 
-CREATE TABLE trip_x_city
+CREATE TABLE journey_x_city
 (
-    id      INT PRIMARY KEY AUTO_INCREMENT,
-    trip_id INT NOT NULL,
-    city_id INT NOT NULL,
-    FOREIGN KEY (trip_id) REFERENCES trip(id),
-    FOREIGN KEY (city_id) REFERENCES city(id)
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    journey_id INT NOT NULL,
+    city_id    INT NOT NULL,
+    CONSTRAINT journey_x_city_journey_fk FOREIGN KEY (journey_id) REFERENCES journey (id),
+    CONSTRAINT journey_x_city_city_fk FOREIGN KEY (city_id) REFERENCES city (id)
 );
